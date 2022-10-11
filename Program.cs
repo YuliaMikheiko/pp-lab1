2b0ceba94eb779c84acfa86fcc213564a07bd248
BlackStreit <mix06062001@bk.ru>
2022-10-11 11:30:35 +0800

    Готово

--- Program.cs
+++ Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+using System.Threading;
 
 namespace ConsoleApp1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int elementCount;
+            int threadCount;
+            Console.WriteLine("Введите кол-во потоков");
+            while (!int.TryParse(Console.ReadLine(), out threadCount)
+                || (threadCount < 4))
+            {
+                Console.WriteLine("Число введено неверно. Попробуйте ещё раз");
+            }
+            Console.WriteLine("Введите кол-во элементов");
+            while (!int.TryParse(Console.ReadLine(), out elementCount)
+                || (elementCount < 1))
+            {
+                Console.WriteLine("Число введено неверно. Попробуйте ещё раз");
+            }
+            int[] array = new int[elementCount];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 16);
+            }
+            int[] returns = new int[threadCount];
+            Thread[] threads = new Thread[threadCount];
+            for (int i = 0; i < threads.Length; i++)
+            {
+
+                threads[i] = new Thread(() => { returns[i] = (ThreadArray(array, i, threadCount)); });
+                threads[i].Start();
+                threads[i].Join();
+
+            }
+            int multOdd = 1;
+            for (int i = 0; i < returns.Length; i++)
+            {
+                multOdd *= returns[i];
+            }
+            Console.WriteLine($"Произведение нечетных элементов: {multOdd}");
+            Console.ReadKey();
+        }
+
+        static int ThreadArray(object? paramArray, object? totalI, object? threadCount)
+        {
+            int beg, h, end;
+            int multOdd = 1;
+            int[]? array = paramArray as int[];
+            int nt = Convert.ToInt32(totalI);
+            int count = Convert.ToInt32(threadCount);
+            h = array.Length / count;
+            beg = h * nt; end = beg + h;
+            if (nt == count - 1)
+            {
+                end = array.Length;
+            }
+            for (int i = beg; i < end; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    multOdd *= array[i];
+                }
+            }
+            return multOdd;
         }
     }
 }
